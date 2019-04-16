644fae50158feb3acf1d86af1d979edf99235faa
wxq <wxq@gmail.com>
2019-04-16 17:12:15 +0800

    test

---
+++ src/main/java/com/controller/test.java
@@ -0,0 +1,47 @@
+package com.controller;
+
+
+import java.io.InputStream;
+
+import com.user;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Controller
+@RequestMapping("/home")
+
+public class test {
+
+    @RequestMapping("/index")
+    public void index(HttpServletRequest request, HttpServletResponse response) {
+
+        //mybatis的配置文件
+        String resource = "mybatis.xml";
+        //使用类加载器加载mybatis的配置文件（它也加载关联的映射文件）
+        InputStream is = test.class.getClassLoader().getResourceAsStream(resource);
+        //构建sqlSession的工厂
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
+        //使用MyBatis提供的Resources类加载mybatis的配置文件（它也加载关联的映射文件）
+        //Reader reader = Resources.getResourceAsReader(resource);
+        //构建sqlSession的工厂
+        //SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(reader);
+        //创建能执行映射文件中sql的sqlSession
+        SqlSession session = sessionFactory.openSession();
+        /**
+         * 映射sql的标识字符串，
+         * me.gacl.mapping.userMapper是userMapper.xml文件中mapper标签的namespace属性的值，
+         * getUser是select标签的id属性值，通过select标签的id属性值就可以找到要执行的SQL
+         */
+        String statement = "me.gacl.mapping.userMapper.getUser";//映射sql的标识字符串
+        //执行查询返回一个唯一user对象的sql
+        user user = session.selectOne(statement, 1);
+        System.out.println(user);
+    }
+
+}
